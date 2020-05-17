8078db14ea2b4a2c8d94ef60b93b6e2e9142a4f1
Szelma139 <raduniek1@gmail.com>
2020-05-17 14:09:17 +0200

    ss

--- src/Components/Geolocation.js
+++ src/Components/Geolocation.js
@@ -1,8 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import { geolocated } from "react-geolocated";
+import Geocode from "react-geocode";
 
 
  const Geolocation = (props) => {
+     const [city, setCity] = useState('')
+     
+    const setUserCity=()=> {
+        Geocode.fromLatLng(props.coords.latitude, props.coords.longitude).then(
+            response => {
+              const address = response.results[0].formatted_address;
+              console.log("DUPAAAA" + address);
+            },
+            error => {
+              console.error("BLAD" + error);
+            }
+          );
+     }
     return !props.isGeolocationAvailable ? (
         <div>Your browser does not support Geolocation</div>
     ) : !props.isGeolocationEnabled ? (
@@ -15,7 +29,8 @@ import { geolocated } from "react-geolocated";
                
                     <p>longitude</p>
                     <p>{props.coords.longitude}</p>
-               
+    <p>{setUserCity()}</p>  
+    <p>{city}</p>
                     <p>altitude</p>
                     <p>{props.coords.altitude}</p>
                 
